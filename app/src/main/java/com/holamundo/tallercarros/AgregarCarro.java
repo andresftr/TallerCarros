239623f47be0e84565ca0d7ab0a85d3ec61164bd
Andres Tarra <afelipe0113@gmail.com>
2018-10-04 20:07:38 -0500

    Interfaz hecha

---
+++ app/src/main/java/com/holamundo/tallercarros/AgregarCarro.java
@@ -0,0 +1,13 @@
+package com.holamundo.tallercarros;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AgregarCarro extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_carro);
+    }
+}
